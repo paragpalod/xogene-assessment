fcf8b335ffdc775bf7a03d3f67997c09ded878a5
Parag Palod <phpalod@gmail.com>
2023-10-12 23:20:04 +0530

    Added loading state for drug details page

--- src/Features/DrugDetails/index.js
+++ src/Features/DrugDetails/index.js
@@ -4,10 +4,12 @@ import AppShell from "../AppShell";
 
 const DrugDetails = () => {
   const { drugName } = useParams();
-  const [drug, setDrug] = useState(null);
+  const [drug, setDrug] = useState([]);
   const [ndc, setNdc] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`https://rxnav.nlm.nih.gov/REST/drugs.json?name=${drugName}`)
       .then((i) => i.json())
       .then((res) => {
@@ -17,33 +19,65 @@ const DrugDetails = () => {
             drugData = i.conceptProperties[0];
           }
         });
-        if(drugData) {
+        if (drugData) {
           setDrug(drugData);
-          fetch(`https://rxnav.nlm.nih.gov/REST/rxcui/${drugData.rxcui}/ndcs.json`).then(i => i.json()).then(ndcRes => {
-            if(ndcRes.ndcGroup?.ndcList?.ndc?.length) {
-              setNdc(ndcRes.ndcGroup.ndcList.ndc);
-            }
-          })
+          fetch(
+            `https://rxnav.nlm.nih.gov/REST/rxcui/${drugData.rxcui}/ndcs.json`
+          )
+            .then((i) => i.json())
+            .then((ndcRes) => {
+              if (ndcRes.ndcGroup?.ndcList?.ndc?.length) {
+                setNdc(ndcRes.ndcGroup.ndcList.ndc);
+              }
+              setLoading(false);
+            });
+        } else {
+          setLoading(false);
         }
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, [drugName]);
 
   return (
     <AppShell heading="DRUG DETAILS">
-      <h2>Name of drug</h2>
-      {drug && (
-        <div className="drug-details">
-          <div><strong>Id:</strong> {drug.rxcui}</div>
-          <div><strong>Name:</strong> {drug.name}</div>
-          <div><strong>Synonym:</strong> {drug.synonym}</div>
-        </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {drug?.length > 0 && (
+            <>
+              <h2>Name of drug</h2>
+              <div className="drug-details">
+                <div>
+                  <strong>Id:</strong> {drug.rxcui}
+                </div>
+                <div>
+                  <strong>Name:</strong> {drug.name}
+                </div>
+                <div>
+                  <strong>Synonym:</strong> {drug.synonym}
+                </div>
+              </div>
+            </>
+          )}
+          {ndc?.length > 0 && (
+            <>
+              <h2>Associated NDCs</h2>
+              <div className="drug-details">
+                {ndc.map((i, index) => (
+                  <div>
+                    <strong>NDC {index + 1}:</strong> {i}
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+          {ndc?.length <= 0 &&  drug?.length <= 0 && <h2>
+            Selected drug details not available</h2>}
+        </>
       )}
-      <h2>Associated NDCs</h2>
-      <div className="drug-details">
-        {ndc.map((i, index) => (
-          <div><strong>NDC {index +1}:</strong> {i}</div>
-        ))}
-      </div>
     </AppShell>
   );
 };
